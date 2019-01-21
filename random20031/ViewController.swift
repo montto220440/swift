8fe246d426209bde9ea8c5bc9eff1eeeb5a80a43
ผู้ใช้ทั่วไป <Guest@macbook-pro-khxng-champis.local>
2019-01-21 19:57:35 +0700

    Initial Commit

---
+++ random20031/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  random20031
+//
+//  Created by ผู้ใช้ทั่วไป on 21/1/2562 BE.
+//  Copyright © 2562 ผู้ใช้ทั่วไป. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ random20031Tests/random20031Tests.swift
@@ -0,0 +1,34 @@
+//
+//  random20031Tests.swift
+//  random20031Tests
+//
+//  Created by ผู้ใช้ทั่วไป on 21/1/2562 BE.
+//  Copyright © 2562 ผู้ใช้ทั่วไป. All rights reserved.
+//
+
+import XCTest
+@testable import random20031
+
+class random20031Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
